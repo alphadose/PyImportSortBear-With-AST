a8a0f7386d14614129f1e1892cfa411b3d8462cf
Alphadose <anish.mukherjee1996@gmail.com>
2018-03-25 15:38:42 +0530

    Added Visitor Implementation

--- sample.py
+++ sample.py
@@ -1,2 +1,9 @@
 from a import b
 from c import d
+import e,f as g
+import h as i
+
+t=1
+print("Random message")
+def random_func():
+	pass
--- traverse.py
+++ traverse.py
@@ -1,16 +1,34 @@
 from antlr4 import *
-from antlr4.tree.Trees import Trees
 from Python2Lexer import Python2Lexer
-from Python2Listener import Python2Listener
+from Python2Visitor import Python2Visitor
 from Python2Parser import Python2Parser
 import sys
+
+class CustomVisitor(Python2Visitor):
+
+	def visitDotted_name(self, ctx:Python2Parser.Import_stmtContext):
+		print("Parent package: " + ctx.getText())
+		return self.visitChildren(ctx)
+
+	def visitDotted_as_name(self, ctx:Python2Parser.Dotted_as_nameContext):
+		info = ctx.getText()
+		if "as" in info:
+			packages = info.split("as")
+			print("Package {} aliased as {}".format(packages[0],packages[1]))
+		return self.visitChildren(ctx)
+
+	def visitImport_from(self, ctx:Python2Parser.Import_as_nameContext):
+		info = ctx.getText()
+		print("Package {} derived from package {}".format(info[-1], info[4]))
+		return self.visitChildren(ctx)
 
 def main():
     lexer = Python2Lexer(FileStream(sys.argv[1]))
     stream = CommonTokenStream(lexer)
     parser = Python2Parser(stream)
     tree = parser.file_input()
-    print(Trees.toStringTree(tree, None, parser))
+    visitor = CustomVisitor()
+    visitor.visit(tree)
 
 if __name__ == '__main__':
     main()
